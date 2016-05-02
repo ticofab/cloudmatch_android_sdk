0b1cc9d411b8f0c783d4ead4516fa1e9cad0d7c4
Fabio Tiriticco <fabio.europe@gmail.com>
2016-05-02 20:11:36 +0200

    bumped grade + minor changes

--- cloudmatch-sdk/src/main/java/io/ticofab/cm_android_sdk/library/models/inputs/GesturePurposeInfo.java
+++ cloudmatch-sdk/src/main/java/io/ticofab/cm_android_sdk/library/models/inputs/GesturePurposeInfo.java
@@ -4,8 +4,8 @@ package io.ticofab.cm_android_sdk.library.models.inputs;
 import io.ticofab.cm_android_sdk.library.consts.GesturePurpose;
 
 public class GesturePurposeInfo {
-    public String mGroupId;
-    public int mMyIdInGroup;
+    public final String mGroupId;
+    public final int mMyIdInGroup;
     public final GesturePurpose mGesturePurpose;
 
     private GesturePurposeInfo(Builder builder) {
